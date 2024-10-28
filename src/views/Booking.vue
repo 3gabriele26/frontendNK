505abe564e22522958cf009340d9ecc1ae53ee60
3gabriele26 <gabrielegiustozzi@protonmail.com>
2024-10-28 13:58:56 +0100

    addign function for form submit

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -89,7 +89,15 @@
         },
         methods: {
             async submitForm() {
-                console.log(this.formData)
+                try {
+                    this.$router.replace('/thanks');
+                    const response = await axios.post('http://127.0.0.1:5000/send-email', this.formData)
+                    //const response = await axios.post('https://elga-af9efdb3b7ea.herokuapp.com/send-email', this.formData) //For debug 
+                    console.log(response)
+                } catch (error) {
+                    console.error('Errore durante l\'invio dell\'email:', error);
+                    alert('Errore durante l\'invio.');
+                }
             }
         }
     }
